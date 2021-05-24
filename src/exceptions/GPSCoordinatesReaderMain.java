410f1993cf3b19ffd66ca66f5d7b087262495a92
Tanguy Pérennou <Tanguy.Perennou@isae-supaero.fr>
2021-05-24 14:47:50 +0200

    Fixed execution error that throws ArrayIndexOutOfBoundsExceptions.

--- src/exceptions/GPSCoordinatesReaderMain.java
+++ src/exceptions/GPSCoordinatesReaderMain.java
@@ -13,9 +13,9 @@ public class GPSCoordinatesReaderMain {
 		String line = reader.readLine(); 
 		while(line != null) { // compilation error fixed by declaring "line" above, not "ligne"
 			String[] words = line.split(",");
-			double lat = Double.parseDouble(words[1]);
-			double lon = Double.parseDouble(words[2]);
-			double alt = Double.parseDouble(words[3]);
+			double lat = Double.parseDouble(words[0]);
+			double lon = Double.parseDouble(words[1]);
+			double alt = Double.parseDouble(words[2]);
 			GPSCoordinates coords = new GPSCoordinates(lat, lon, alt);
 			System.out.println(coords);
 			line = reader.readLine();
