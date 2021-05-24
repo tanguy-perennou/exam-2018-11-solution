ec1d77b051ebddd378ef94808c7fa6583f6c6408
Tanguy Pérennou <Tanguy.Perennou@isae-supaero.fr>
2021-05-24 14:38:58 +0200

    Fixed all 'unused variable' compilation warnings

--- src/exceptions/GPSCoordinatesReaderMain.java
+++ src/exceptions/GPSCoordinatesReaderMain.java
@@ -8,7 +8,7 @@ public class GPSCoordinatesReaderMain {
 	
 	public static void main(String[] args) throws IOException {
 		FileReader fileReader = new FileReader("src/exceptions/summits.txt");
-		BufferedReader reader = new BufferedReader(null);
+		BufferedReader reader = new BufferedReader(fileReader);
 		
 		String line = reader.readLine(); 
 		while(line != null) { // compilation error fixed by declaring "line" above, not "ligne"
@@ -16,11 +16,12 @@ public class GPSCoordinatesReaderMain {
 			double lat = Double.parseDouble(words[1]);
 			double lon = Double.parseDouble(words[2]);
 			double alt = Double.parseDouble(words[3]);
-			GPSCoordinates coords = new GPSCoordinates(lat, lat, lat);
+			GPSCoordinates coords = new GPSCoordinates(lat, lon, alt);
 			System.out.println(coords);
 			line = reader.readLine();
 		}
 		
+		//reader.close();
 		
 	}
 
