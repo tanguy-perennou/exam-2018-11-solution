d6bcbeeb78efc23c92482f923b59f68fd377ae0e
Tanguy Pérennou <Tanguy.Perennou@isae-supaero.fr>
2021-05-24 14:57:47 +0200

    Added comments for all error fixes

--- src/exceptions/GPSCoordinatesReaderMain.java
+++ src/exceptions/GPSCoordinatesReaderMain.java
@@ -6,22 +6,22 @@ import java.io.IOException;
 
 public class GPSCoordinatesReaderMain {
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException { // compilation error on lines 10 et 13 fixed by adding throws clause
 		FileReader fileReader = new FileReader("src/exceptions/summits.txt");
-		BufferedReader reader = new BufferedReader(fileReader);
+		BufferedReader reader = new BufferedReader(fileReader); // compilation warning on line 10 fixed by initializing reader with fileReader
 		
-		String line = reader.readLine(); 
-		while(line != null) { // compilation error fixed by declaring "line" above, not "ligne"
-			String[] words = line.split(",");
-			double lat = Double.parseDouble(words[0]);
+		String line = reader.readLine(); // compilation error on lines 14, 15 and 21 fixed by declaring "line" instead of "ligne"
+		while(line != null) { 
+			String[] words = line.split(","); // will produce a 3-element array (see any line in summits.txt)
+			double lat = Double.parseDouble(words[0]); // use index 0 for first element
 			double lon = Double.parseDouble(words[1]);
-			double alt = Double.parseDouble(words[2]);
-			GPSCoordinates coords = new GPSCoordinates(lat, lon, alt);
+			double alt = Double.parseDouble(words[2]); // fixes exception thrown at execution time when using index 3
+			GPSCoordinates coords = new GPSCoordinates(lat, lon, alt); // use the three above variables
 			System.out.println(coords);
 			line = reader.readLine();
 		}
 		
-		reader.close();
+		reader.close(); // compilation warning on line 11 fixed by closing reader after use
 	}
 
 }
